ecce10cb9ac322dfb03fb2961c3145ba111ab094
Jeroen Peters <jeroenpeters1986@gmail.com>
2022-12-30 14:06:26 +0100

    Small code fixes

--- src/Tags/Likebox.php
+++ src/Tags/Likebox.php
@@ -19,25 +19,16 @@ class Likebox extends Tags
         $width = $this->params->get('width', '240');
         $height = $this->params->get('height', 'auto');
         $language = $this->params->get('lang', 'en_US');
-        $show_header = $this->params->get('show_header', 'true');
         $compact_header = $this->params->get('compact_header', 'false');
         $hide_cover = $this->params->get('hide_header_cover', 'true');
         $content_tabs = $this->params->get('content_tabs', 'false');
 
-        /** Jankie jankie */
-        /**
-            if (!empty($this->facebook_height) && $this->facebook_height != "auto') {
-                echo 'data-height="' . $this->facebook_height . '"' . "\n";
-            }?>
-         **/
-
-
-        $html = "<div class='fb-page'
+        return "<div class='fb-page'
              data-href='https://www.facebook.com/{$page_name}'
              data-width='{$width}'
              data-height='{$height}'
              data-tabs='{$content_tabs}'
-             data-hide-cover='{$content_tabs}'
+             data-hide-cover='{$hide_cover}'
              data-small-header='{$compact_header}'></div>
         <div id='fb-root'></div>
         <script>(function(d, s, id) {
@@ -47,7 +38,5 @@ class Likebox extends Tags
             js.src = '//connect.facebook.net/{$language}/all.js#xfbml=1&appId=';
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>";
-
-        return $html;
     }
 }
